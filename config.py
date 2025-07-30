62fce782ab199ceb320edcbe5d0f9c22ae678dff
gcarloslima <carlosrelgs@gmail.com>
2025-07-29 22:23:35 -0300

    teste parâmetros

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import torch
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 NUM_EPOCHS = 30
-LEARNING_RATE = 0.002
+LEARNING_RATE = 0.001
 IMAGE_SIZE = (256, 256)
 NUM_CLASSES = 120
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
